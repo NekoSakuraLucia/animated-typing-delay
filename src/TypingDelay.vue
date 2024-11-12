f01e0c0dee3fd68d2d2d287f9eb2fdac6cfd37a2
NekoSakuraLucia <mokurolucia@gmail.com>
2024-11-12 17:05:25 +0700

    เพิ่มให้มี wrapper และ easing

--- src/TypingDelay.vue
+++ src/TypingDelay.vue
@@ -1,27 +1,37 @@
 <template>
-    <span>{{ displayText }}</span>
+    <component :is="wrapper" :style="{ transition: `all ${typingSpeed}ms ${easing}` }">
+        {{ displayText }}
+    </component>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const props = defineProps({
-  text: {
-    type: Array,
-    required: true
-  },
-  typingSpeed: {
-    type: Number,
-    default: 100
-  },
-  eraseDelay: {
-    type: Number,
-    default: 5000
-  }
+    text: {
+        type: Array,
+        required: true
+    },
+    typingSpeed: {
+        type: Number,
+        default: 100
+    },
+    eraseDelay: {
+        type: Number,
+        default: 5000
+    },
+    wrapper: {
+        type: String,
+        default: "span",
+    },
+    easing: {
+        type: String,
+        default: 'ease-in-out',
+        validator(value) {
+            return ["ease-in", "ease-out", "ease-in-out", "ease-out-in", "ease-out-cubic"].includes(value);
+        }
+    }
 });
-
-const typingSpeed = props.typingSpeed;
-const eraseDelay = props.eraseDelay;
 
 const displayText = ref("");
 const index = ref(0);
@@ -35,19 +45,19 @@ const typeText = () => {
             displayText.value += props.text[index.value][charIndex.value];
             charIndex.value++;
         } else {
-            isErasing.value=true;
+            isErasing.value = true;
         }
     } else {
         if (charIndex.value > 0) {
-            displayText.value = displayText.value.slice(0, 1);
+            displayText.value = displayText.value.slice(0, -1);
             charIndex.value--;
         } else {
-            isErasing.value =false;
+            isErasing.value = false;
             index.value++;
             if (index.value >= props.text.length) index.value = 0;
         }
     }
 };
 
-setInterval(typeText, typingSpeed);
+setInterval(typeText, props.typingSpeed);
 </script>
